c06fe16909d31c99fc74e37abadc854ed756bb15
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-06-02 22:41:52 +0900

    routesを修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   }
 
   namespace :auth do
-    resources :sessions, only: %i[index]
+    resources :sessions, only: [:index]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
