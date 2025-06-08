9393abbd74350a10f73954f272b815f4d2688889
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-06-08 22:37:38 +0900

    validationエラーに対応

--- app/models/work_time.rb
+++ app/models/work_time.rb
@@ -1,11 +1,9 @@
 class WorkTime < ApplicationRecord
   belongs_to :user
-  belongs_to :approved_by_id, class_name: User, optional: true
-  belongs_to :job, presence: true
+  belongs_to :approved_by_id, class_name: "User", optional: true
+  belongs_to :job, optional: true
 
   validates :work_date, presence: true
-  validates :clock_in
-  validates :clock_out
   validates :work_minute, presence: true
   validates :break_duration_minute, presence: true
   validates :approved, presence: true
