b32def1a95fb52f94b44917a3eca87f23a6a5a50
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-06-09 22:20:40 +0900

    時刻から労働時間を算出。休憩時間を分単位に変換。

--- app/controllers/work_times_controller.rb
+++ app/controllers/work_times_controller.rb
@@ -7,10 +7,20 @@ class WorkTimesController < ApplicationController
   end
 
   def create
-    work_time = current_user.work_times.create!(work_time_params)
-    render json: work_time
-  rescue ActiveRecord::RecordInvalid => e
-    render json: { errors: e.record.errors }, status: :unprocessable_entity
+    work_time = current_user.work_times.build(work_time_params)
+    work_time.break_duration_minute = params[:break_duration_minute].split(':').map(&:to_i).then { |hours, minutes| hours * 60 + minutes }
+
+    if params[:clock_in].present? && params[:clock_out].present?
+      clock_in = Time.parse(params[:clock_in])
+      clock_out = Time.parse(params[:clock_out])
+      work_time.work_minute = ((clock_out - clock_in) / 60).to_i - work_time.break_duration_minute
+    end
+
+    if work_time.save
+      render json: work_time, status: :created
+    else
+      render json: { errors: work_time.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
