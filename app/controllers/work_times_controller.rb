dac22a4fe94fc0564b2acd9c434a2a0321d809ed
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-06-16 22:52:18 +0900

    変数名を修正

--- app/controllers/work_times_controller.rb
+++ app/controllers/work_times_controller.rb
@@ -7,19 +7,19 @@ class WorkTimesController < ApplicationController
   end
 
   def create
-    work_time = current_user.work_times.build(work_time_params)
-    work_time.break_duration_minute = params[:break_duration_minute].split(':').map(&:to_i).then { |hours, minutes| hours * 60 + minutes }
+    work_times = current_user.work_times.build(work_time_params)
+    work_times.break_duration_minute = params[:break_duration_minute].split(':').map(&:to_i).then { |hours, minutes| hours * 60 + minutes }
 
     if params[:clock_in].present? && params[:clock_out].present?
       clock_in = Time.parse(params[:clock_in])
       clock_out = Time.parse(params[:clock_out])
-      work_time.work_minute = ((clock_out - clock_in) / 60).to_i - work_time.break_duration_minute
+      work_times.work_minute = ((clock_out - clock_in) / 60).to_i - work_times.break_duration_minute
     end
 
-    if work_time.save
-      render json: work_time, status: :created
+    if work_times.save
+      render json: work_times, status: :created
     else
-      render json: { errors: work_time.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: work_times.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
@@ -38,7 +38,7 @@ class WorkTimesController < ApplicationController
     if work_time
       render json: work_time
     else
-      render json: { error: 'Work time not found' }, status: :not_found
+      render json: { error: 'Work times not found' }, status: :not_found
     end
   end
 
@@ -52,7 +52,9 @@ class WorkTimesController < ApplicationController
       :break_duration_minute,
       :note,
       :approved,
-      :approved_by_id
+      :approved_by_id,
+      :overtime_work_minute,
+      :id_paid_holiday
     )
   end
 end
