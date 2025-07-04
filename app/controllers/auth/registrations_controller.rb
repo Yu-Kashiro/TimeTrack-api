a1bf59b71012b42553f3d227782df53ebb607457
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-07-04 17:12:47 +0900

    説明書きを追加

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -1,6 +1,10 @@
 class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
 
+  # 標準のメソッドは以下のとおり
+  # https://github.com/lynndylanhurley/devise_token_auth/blob/master/app/controllers/devise_token_auth/registrations_controller.rb
+
+  # 標準のDeviseTokenAuthには、:name パラメータが存在しないため、sign_up_paramsを上書き。
   def sign_up_params
     params.permit(:email, :password, :password_confirmation, :name)
   end
